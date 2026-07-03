d4a6dd7bacd19b1012c606476418495963d82880
perry <perry@perrys-MacBook-Air.local>
2026-07-04 05:17:03 +0700

    fix: bind Next.js to 0.0.0.0 to allow access from other computers

--- web/scripts/run-web.sh
+++ web/scripts/run-web.sh
@@ -4,5 +4,5 @@ set -eu
 cd "$(dirname "$0")/.."
 
 exec ./node_modules/.bin/next start \
-  --hostname "${SYNCPOST_HOST:-127.0.0.1}" \
+  --hostname "${SYNCPOST_HOST:-0.0.0.0}" \
   --port "${PORT:-3000}"
